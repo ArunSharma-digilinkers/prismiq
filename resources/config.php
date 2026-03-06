9b76658f2be66aa8a9ffaea8d2fe2b3c5629ce6e
Arun Sharma <digilinkers@gmail.com>
2026-03-06 14:53:30 +0530

    updations on Home page

--- resources/config.php
+++ resources/config.php
@@ -12,7 +12,7 @@ defined('TEMPLATE_FRONT') ? null : define('TEMPLATE_FRONT', __DIR__ . DS . 'temp
 $company = "Prismiq";
 
 // Localhost settings
-$path = '/prismiq/public/';
+$path = '/projects/prismiq/public/';
 
 
 defined('DB_HOST') ? null : define('DB_HOST', 'localhost');
